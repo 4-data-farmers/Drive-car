b5ccaa4ac4d9d059b461e1cd0cfaf5583b3c96d2
wxken <1527178852@qq.com>
2019-10-30 17:51:34 +0800

    Auto stash before merge of "master" and "origin/master"

--- GDmap/views.py
+++ GDmap/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render,redirect
 
-import requests
 import json
-
+# 6565161651656
 # Create your views here.
 
 def index1(request):
     if request.method == 'GET':
-        return render(request,'index.html')
+        return render(request, 'index.html')
 
 def logins(request):
     if request.method == 'GET':
